4c16959e3ba6fb369623f51335fb48dbbf4a63a2
K-ARIVAZHAGAN <arivazhagan24012004@gmail.com>
2025-11-17 18:33:38 +0530

    add or even

---
+++ Addoreven.java
@@ -0,0 +1,12 @@
+class Addoreven{
+    public  static void main(String[] args){
+        int number = 10;
+        if(number % 2 == 0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+        
+    }
+}
+
